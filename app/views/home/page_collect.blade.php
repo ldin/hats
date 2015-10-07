e8320736251d4d0ee2089d3685ce531af8733581
ldin <ldinka.alena@gmail.com>
2015-10-07 14:58:41 +0300

    collect

--- app/views/home/page_collect.blade.php
+++ app/views/home/page_collect.blade.php
@@ -9,8 +9,10 @@
     <div id="collect">
         
             @if(!empty($type->text) && empty($row))
-                <div class="container">
-                    {{ $type->text }}
+                <div class="container"> 
+                    <div class="col-xs-12 col-sm-9 col-sm-offset-3">
+                        {{ $type->text }}
+                    </div>    
                 </div>        
             @endif
 
@@ -21,34 +23,16 @@
                         {{ $row->text }}
                     </div>    
                 @else
-<!--                     <div id="brend-line">
-                        <div class="container">
-                            <ul>
-                                <li><a href="#tavitta"><img alt="tavitta" src="/img/collect/collectt_ico_1.png" /></a></li>
-                                <li><a href="#gerda"><img alt="gerda" src="/img/collect/collectt_ico_2.png" /></a></li>
-                                <li><a href="#estro"><img alt="estro" src="/img/collect/collectt_ico_3.png" /></a></li>
-                                <li><a href="#swarovski"><img alt="swarovski" src="/img/collect/collectt_ico_4.png" /></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-xs-12 col-sm-9 col-sm-offset-3 ">
-                                <br>
-                                <p>У нас можно преобрести</p>
-                                <p><strong>оптом, по ценам производителей,</strong></p>
-                                <p>женские головные уборы следующих брендов:</p>
-                            </div>
-                        </div>
-                        <hr>
-                    </div> -->
+
                 @endif
                 @foreach($posts as $post)
                     <!-- текст поста -->
                     <div class="container">
                         <div class="block-post row ">
                             <div class="col-xs-12 col-sm-3 post-logo">
-                                {{ HTML::image($post->image, $post->name) }}
+                                @if(!empty($post->image))
+                                    {{ HTML::image($post->image, $post->name) }}
+                                @endif
                             </div>                    
 
                             <div class="col-xs-12 col-sm-9 ">
@@ -61,33 +45,41 @@
                         </div>
                     </div>
                     <!-- изображения поста -->
+                    @if(!empty($post->gallery[0]) )
                     <div class="galleries">
                         <div class="container">
                             <div class="row gallery">
-                                @if(!empty($post->gallery[0]) )
-                                    @foreach($post->gallery as $image)
-                                        <div class="col-xs-6 col-sm-3 gallery-cell">
-                                            <div class="gallery-image">
-                                                <div class="image">
-                                                    {{ HTML::image($image->small_image, $image->alt) }}
-                                                </div>
-                                                <div class="description">
-                                                    {{ $image->text }}
-                                                </div>
+                                @foreach($post->gallery as $image)
+                                    <div class="col-xs-6 col-sm-3 gallery-cell">
+                                        <div class="gallery-image">
+                                            <div class="image">
+                                                {{ HTML::image($image->small_image, $image->alt) }}
+                                            </div>
+                                            <div class="description">
+                                                {{ $image->text }}
                                             </div>
                                         </div>
-                                    @endforeach
-                                @endif
+                                    </div>
+                                @endforeach                                
                             </div>
                             <div class="row text-center gallery-contact">
-                                <!-- <p> Еще</p> -->
                                 <p>
-                                    <a href="#" class="btn btn-border">Условия поставки</a>
+                                    <a href="#" >Условия поставки</a>
                                     <a href="#" class="btn btn-solid">ЗАКАЗАТЬ КАТАЛОГ</a>
                                 </p>
                             </div>
                         </div>
-                    </div>    
+                    </div>
+                    @else
+                        <div class="container">
+                            <div class="row text-center gallery-contact">
+                                <p>
+                                    <a href="#">Условия поставки</a>
+                                    <a href="#" class="btn btn-solid">ЗАКАЗАТЬ КАТАЛОГ</a>
+                                </p>
+                            </div>
+                        </div>
+                    @endif    
 
                 @endforeach
 

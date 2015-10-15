7094c0d2bf0ab8c07a69515f18b553fb4c5c1739
ldin <ldinka.alena@gmail.com>
2015-10-15 11:50:16 +0300

    gallery image admin

--- app/views/admin/post-gallery.blade.php
+++ app/views/admin/post-gallery.blade.php
@@ -1,7 +1,7 @@
 <div class="gallery" id="gallery">
 @if(!empty($row->id))
 	<br><br>
-	<h3><i class="glyphicon glyphicon-th-large"></i> Галлерея изображений</h3>
+	<h3><i class="glyphicon glyphicon-th-large"></i> Галерея изображений</h3>
 
 	@if(!empty($galleries))
 		@foreach($galleries as $image)
@@ -79,12 +79,7 @@
 		            <div class="form-group col-sm-4 col-xs-12">
 		                {{ Form::label('inputImage', 'Изображение') }}
 		                {{ Form::file('image') }}
-		                @if(isset($row->image)&&($row->image))
-		                    <br>
-		                    <div class="img img-thumbnail">
-		                        {{ HTML::image($row->image, 'img') }}
-		                    </div>
-		                @endif
+
 		            </div>
 		        </div>
 	        	{{ Form::submit('Добавить', array( 'class' => 'btn btn-success')) }}

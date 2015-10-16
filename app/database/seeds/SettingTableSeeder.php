1fc22099c29b0037df7806e2d44fdc8859ad0d8b
ldin <ldinka.alena@gmail.com>
2015-10-16 10:27:20 +0300

    setting edit

--- app/database/seeds/SettingTableSeeder.php
+++ app/database/seeds/SettingTableSeeder.php
@@ -7,12 +7,10 @@ class SettingTableSeeder extends Seeder {
     DB::table('settings')->delete();
 
     DB::table('settings')->insert(array(
-      array( 'name' => 'phone', 'title'=>'phone', 'value'=>'', ),
-      array( 'name' => 'email', 'title'=>'email', 'value'=>'', ),
       array( 'name' => 'title', 'title'=>'title', 'value'=>'', ),
-      array( 'name' => '', 'title'=>'', 'value'=>'', ),
-      array( 'name' => '', 'title'=>'', 'value'=>'', ),
-      array( 'name' => '', 'title'=>'', 'value'=>'', ),
+      array( 'name' => 'phone_head', 'title'=>'Телефон в шапке', 'value'=>'', ),
+      array( 'name' => 'email_head', 'title'=>'email в шапке', 'value'=>'', ),
+      array( 'name' => 'email', 'title'=>'email для отправки формы', 'value'=>'', ),
     ));
   }
 
